6642ec19e185f46a8521c25a1179e657f5a5f57e
faisal afif <faisalalva66@gmail.com>
2019-12-05 15:53:09 +0700

    tambahduaangkamodifid

--- belajar-java/TambahDuaAngka.java
+++ belajar-java/TambahDuaAngka.java
@@ -1,5 +1,5 @@
 public class TambahDuaAngka{
     public static void main(String[] args){
-        System.out.println(Integer.parseInt(args[0]+Integer.parseInt[1]));
+        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
     }
 }
